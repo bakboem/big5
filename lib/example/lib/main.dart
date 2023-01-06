c1edccb8b39e165e03a45cc4fa50c2c7b02ad057
bakbeom <bakbeom@bakbeom.local>
2023-01-06 19:58:44 +0900

    0.0.2 added example code

---
+++ lib/example/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_big5/big5.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Big5 Convert'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+var str = '國立臺灣大學';
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(Big5.encode(str).toString()),
+              const Divider(),
+              Text(Big5.decode(Big5.encode(str))),
+            ],
+          ),
+        ) // This trailing comma makes auto-formatting nicer for build methods.
+        );
+  }
+}
